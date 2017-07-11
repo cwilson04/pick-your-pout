ea7b38d2e5c7db716bc8310d2caf9373ea31addd
Ryan Milstead <ryanmilstead1@gmail.com>
2017-07-11 13:55:31 -0400

    add devises to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :questions,only:[:show]
 
   get 'welcome/index'
 
   get 'welcome/about'
-  
+
   get 'welcome/getstarted'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
